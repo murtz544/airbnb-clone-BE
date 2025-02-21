18f48231acb270c457c703025fc2377f65b0b39b
Muhammad Murtaza Hassan <136434291+murtz544@users.noreply.github.com>
2025-02-21 19:41:51 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use(express.json());
 //   app.use(cors());
 // }
 app.use(cors({
-  origin: ["https://airbnb-fe-8288.onrender.com", "http://localhost:5173"], 
+  origin: ["https://airbnb-fe-8288.onrender.com", "http://localhost:5173", "https://airbnb-clone-frontend-ftw8.onrender.com"], 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
